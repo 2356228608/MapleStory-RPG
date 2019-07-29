78a74e3e286d54d7ac740b29e6bd82960a3fc6f9
Hongyu Wang <124439607@qq.com>
2019-07-29 13:40:55 +0800

    Update 30004.js

--- 冒险岛：传说世界/任务/30004.js
+++ 冒险岛：传说世界/任务/30004.js
@@ -1,29 +1,78 @@
-﻿/* 鲁塔比斯
+﻿/* 鲁塔比斯 逃出巨大的树根2
 Made by Jessefjxm
  */
 var status = -1;
 
 function start(mode, type, selection) {
-	if (mode == 1)
-		status++;
-	else
-		status--;
-	if (status == 0) {
-		qm.sendNext("出口真的没有问题了吗？", 1064001);
-	} else if (status == 1) {
-		qm.sendNext("你能带我离开这里了吗？", 1064001);
-	} else if (status == 2) {
-		qm.sendNextS("我找到附近的出口了，你看（指）。");
-	} else if (status == 3) {
-		qm.sendNext("太好了！但是……那个地方该不会被堵住了吧？", 1064001);
-	} else if (status == 4) {
-		qm.sendNextS("好吧，我先自己过去看看。");
-	} else if (status == 5) {
-		qm.sendNextS("#r嗯，看上去这个出口完全没有问题嘛。");
-	} else if (status == 6) {
-		qm.sendNextS("嘿，这个出口是安全的，完全可以从这里离开鲁塔比斯。");
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		qm.dispose();
+	} else if (status === i++) {
+		qm.sendNext("看样子利用出口逃出去是不可能了。", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("那该怎么办？");
+	} else if (status === i++) {
+		qm.sendAcceptDecline("这是我来这里时带来的卷轴。据说可以用它移动到附近的村庄，我才带的。可是我使用后却什么变化也没有。估计是假货。你帮我瞧瞧吧。", 1064001);
+	} else if (status === i++) {
+		qm.sendNext("给，这是卷轴！", 1064001);
+	} else if (status === i++) {
+		qm.gainItem(2431151,1);
+		// 收尾		
 		qm.forceStartQuest();
+		// 这个还没完成
+		//qm.forceCompleteQuest();
+		qm.dispose();
+	} else {
+		qm.dispose();
+	}
+}
+
+// 传送到 910700000
+function 后续action(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		ms.dispose();
+	} else if (status === i++) {
+		// 初始化
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(true, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
+		ms.inGameDirectionEvent_AskAnswerTime(3000);
+	} else if (status === i++) {		
+		ms.sendNextSNoESC("虽然卷轴很陈旧，但是一点问题都没有。重新回去吧。");
+	} else if (status === i++) {
+		ms.inGameDirectionEvent_AskAnswerTime(3000);
+	} else if (status === i++) {
+		// 收尾
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(false, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
+		ms.dispose();
+		// 传送到 巨大的树根	
+		ms.warp(910700200, 0);
+	} else {
+		ms.dispose();
+	}
+}
+
+// 说不定不需要？
+function end(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		qm.dispose();
+	} else if (status === i++) {
+		qm.sendNext("确认了吗？", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("这次的卷轴也什么问题都没有。");
+	} else if (status === i++) {
+		qm.sendNext("这次又失败了……我真的没办法出去吗……？", 1064001);
+	} else if (status === i++) {
 		qm.forceCompleteQuest();
+		qm.dispose();
+	} else {
 		qm.dispose();
 	}
 }
