ec17fd3f5ce1d06e92ef9ee19b4c29c691e0c2d8
Hongyu Wang <124439607@qq.com>
2019-07-29 13:06:21 +0800

    Update root_meet.js

--- 冒险岛：传说世界/地图/onUserEnter/root_meet.js
+++ 冒险岛：传说世界/地图/onUserEnter/root_meet.js
@@ -1,18 +1,42 @@
-/* Dawnveil
-[Root Abyss] An Urgent Summons
-Neinheart
+/* 鲁塔比斯
 Made by Jessefjxm
  */
 var status = -1;
 
 function action(mode, type, selection) {
-	if (mode == 0) {
-		status--;
-	} else {
-		status++;
-	}
+	status++;
 	if (ms.isQuestActive(30000)) {
-		ms.forceCompleteQuest(30000);
+		action30000(mode, type, selection);
 	}
-	ms.dispose();
+}
+
+function action30000(mode, type, selection) {
+	var i = -1;
+	if (status <= i++) {
+		ms.dispose();
+	} else if (status === i++) {
+		// 初始化
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(true, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
+	} else if (status === i++) {
+		// 文字淡入 黑体 起来…起来……
+		ms.inGameDirectionEvent_AskAnswerTime(3000);
+	} else if (status == i++) {
+		// 文字淡出
+		ms.inGameDirectionEvent_AskAnswerTime(3000);
+	} else if (status == i++) {
+		// 放动画 睁眼
+		ms.inGameDirectionEvent_AskAnswerTime(10000); 
+	} else if (status === i++) {
+		// 收尾
+		ms.forceCompleteQuest(30000);
+		ms.curNodeEventEnd(true);
+		// ! 上下黑屏还是要保留，不知道怎么操作
+		ms.setInGameDirectionMode(false, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
+		ms.dispose();
+	} else {
+		ms.dispose();
+	}
 }
