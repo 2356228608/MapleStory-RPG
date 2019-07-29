627d54b8eb750a002384dbc10bbd0a01941c454b
Hongyu Wang <124439607@qq.com>
2019-07-29 13:23:50 +0800

    Update 30002.js

--- 冒险岛：传说世界/任务/30002.js
+++ 冒险岛：传说世界/任务/30002.js
@@ -1,23 +1,85 @@
-﻿/* 鲁塔比斯
+﻿/* 鲁塔比斯 奇怪的少女
 Made by Jessefjxm
  */
 var status = -1;
 
 function start(mode, type, selection) {
-	if (mode == 1)
-		status++;
-	else
-		status--;
-	if (status == 0) {
-		qm.sendNextS("这里是哪？这个少女又是谁？先跟她说话看看。");
-	} else if (status == 1) {
-		qm.sendNext("救救我！带我出去，离开这个地方！", 1064001);
-	} else if (status == 2) {
-		qm.sendNextS("好……好吧，你不要激动，让我看看。");
-	} else if (status == 3) {
-		qm.sendNextS("#r这个少女真是奇怪，一上来就不由分说地让我带她出去，离开这个地方。我还没搞清楚状况就稀里糊涂地答应了。先来想想怎么带她出去吧。");
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		qm.dispose();
+	} else if (status === i++) {
+		qm.sendNext("我想离开这里。", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("你说什么？");
+	} else if (status === i++) {
+		qm.sendNext("我想从这里出去。", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("你到底在说什么啊？这里是什么地方？你是谁？");
+	} else if (status === i++) {
+		qm.sendNext("这里？这里是鲁塔比斯。我想离开这里。请你帮帮我。", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("（唉……一直在自言自语。真费劲。）");
+	} else if (status === i++) {
+		qm.sendYesNoS("（看来她好像是迷路了。要帮帮她吗？）");
+	} else if (status === i++) {
+		qm.sendNextS("知道了。我来看看有没有办法离开这里。");
+	} else if (status === i++) {
+		qm.sendNext("你真的愿意帮我吗？不许骗我哦！", 1064001);
+	} else if (status === i++) {
+		qm.sendNextS("嗯，肯定在某处有出去的路……");
+	} else if (status === i++) {
+		// 镜头移动到最右边
+	} else if (status === i++) {
+		qm.inGameDirectionEvent_AskAnswerTime(4000);
+	} else if (status === i++) {
+		// 镜头移动到最左边
+	} else if (status === i++) {
+		qm.inGameDirectionEvent_AskAnswerTime(4000);
+	} else if (status === i++) {
+		// 镜头移动回玩家
+	} else if (status === i++) {
+		qm.inGameDirectionEvent_AskAnswerTime(3000);
+	} else if (status === i++) {
+		// 收尾		
 		qm.forceStartQuest();
+		// 这个还没完成
+		//qm.forceCompleteQuest();
+		qm.dispose();
+	} else {
+		qm.dispose();
+	}
+}
+
+// 左边传送门触发
+function start(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		qm.dispose();
+	} else if (status === i++) {
+		qm.sendNextS("果然有出口。应该把这一事实告诉少女。");
+	} else if (status === i++) {
+		// 客户端干活？
+		//qm.forceCompleteQuest(30002);
+		qm.dispose();
+	} else {
+		qm.dispose();
+	}
+}
+
+// 说不定不需要？
+function end(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		qm.dispose();
+	} else if (status === i++) {
+		qm.sendNext("你找到离开这里的办法了吗？", 1064001);
+	} else if (status === i++) {
 		qm.forceCompleteQuest();
+		qm.dispose();
+	} else {
 		qm.dispose();
 	}
 }
