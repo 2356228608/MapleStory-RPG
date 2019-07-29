4f433c915be0255789df69cfea3808d64ac35547
jessefjxm <124439607@qq.com>
2019-07-29 13:36:16 +0800

    Create 2430339.js

---
+++ 冒险岛：传说世界/道具/2430339.js
@@ -0,0 +1,10 @@
+﻿/* 
+ * 2431390 - 坐骑 跑车 永久
+ */
+        var period = 90;
+var mountSkillId = 80001032;
+
+function start() {
+    im.giveMountSkill(im.getItem(), mountSkillId);
+    im.dispose();
+}
