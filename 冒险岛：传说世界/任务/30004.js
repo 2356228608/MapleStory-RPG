b078382f218c4fc1cda203da57569b7ac7b5f7ec
Hongyu Wang <124439607@qq.com>
2019-07-30 12:53:08 +0800

    Update 30004.js

--- 冒险岛：传说世界/任务/30004.js
+++ 冒险岛：传说世界/任务/30004.js
@@ -23,11 +23,11 @@ function end(mode, type, selection) {
 	if (status <= i++) {
 		qm.dispose();
 	} else if (status === i++) {
-		qm.sendNext("确认了吗？", 1064001);
+		qm.sendNextNoESC("确认了吗？", 1064001);
 	} else if (status === i++) {
-		qm.sendNextS("这次的卷轴也什么问题都没有。");
+		qm.sendNextSNoESC("这次的卷轴也什么问题都没有。");
 	} else if (status === i++) {
-		qm.sendNext("这次又失败了……我真的没办法出去吗……？", 1064001);
+		qm.sendNextNoESC("这次又失败了……我真的没办法出去吗……？", 1064001);
 	} else if (status === i++) {
 		qm.forceCompleteQuest();
 		qm.dispose();
