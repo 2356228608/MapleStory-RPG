5afa22e976fffdf7e0dc8a9551f1c98a592bb9c7
jessefjxm <124439607@qq.com>
2019-08-01 16:04:05 +0800

    Update 测试.js

--- 特殊/测试.js
+++ 特殊/测试.js
@@ -13,86 +13,14 @@ function start() {
 	action(1, 0, 0);
 }
 
+// 主体
+
+// 剧情专用单向事件
 function action(mode, type, selection) {
 	status++;
-	var i = -1;
-	if (status <= i++) {
-		cm.dispose();
-	} else if (status === i++) {
-		// 初始化
-		//cm.curNodeEventEnd(true);
-		cm.setInGameDirectionMode(true, false); //屏蔽/解锁操作台 true = 锁 false = 解
-		cm.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
-		cm.inGameDirectionEvent_AskAnswerTime(30);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
+	if (status < 10) {
+		cm.getMap().startMapEffect(status + 5120001, status + 51200001, 1);
 		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		cm.startMapEffect(""+5120001+status,5120001+status,1);
-		cm.inGameDirectionEvent_AskAnswerTime(1000);
-	} else if (status === i++) {
-		// 收尾
-		//cm.curNodeEventEnd(true);
-		cm.setInGameDirectionMode(false, false); //屏蔽/解锁操作台 true = 锁 false = 解
-		cm.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
-		cm.dispose();
 	} else {
 		cm.dispose();
 	}
