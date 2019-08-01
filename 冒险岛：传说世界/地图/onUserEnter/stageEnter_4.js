419a32c2a42e38d10f091e4a7e2efafbc431d280
Hongyu Wang <124439607@qq.com>
2019-08-01 13:42:49 +0800

    Create stageEnter_4.js

---
+++ 冒险岛：传说世界/地图/onUserEnter/stageEnter_4.js
@@ -0,0 +1,35 @@
+/* 起源之塔
+Made by Jessefjxm
+ */
+var status = -1;
+
+function action(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		ms.dispose();
+	} else if (status === i++) {
+		// 初始化
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(true, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
+		ms.inGameDirectionEvent_AskAnswerTime(30);
+	} else if (status === i++) {
+		ms.fieldEffect_InsertCanvas(1, 128, 0, 0, 0, 1000, 0);
+		ms.inGameDirectionEvent_AskAnswerTime(1000);
+	} else if (status === i++) {
+		ms.inGameDirectionEvent_Monologue("#fn黑体##fs32#B - 04 F\r\n\r\n#fs22#这个……\r\n\r\n以中间的峡谷为中心，要使右侧的峭壁和左侧的峭壁保持均衡！\r\n\r\n根据状态需要往返于左侧和右侧的峭壁消灭怪物，以保持两侧的均衡。\r\n\r\n在越过峡谷时一定要小心机关和深渊。", 30000);
+	} else if (status === i++) {
+		// 收尾
+		ms.fieldEffect_InsertCanvas(0, 0, 0, 0, 1000, 0, 0);
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(false, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
+		ms.dispose();
+		// TODO
+		ms.getMap().startMapEffect("请保持左右两侧怪物的平衡。", 5120085);
+		ms.warp(992004000, 1);
+	} else {
+		ms.dispose();
+	}
+}
