a609af1e674bb4c8e1b15eef3d1c8d1e2b198e47
Hongyu Wang <124439607@qq.com>
2019-08-01 13:57:24 +0800

    Create stageEnter_7.js

---
+++ 冒险岛：传说世界/地图/onUserEnter/stageEnter_7.js
@@ -0,0 +1,53 @@
+/* 起源之塔
+Made by Jessefjxm
+ */
+var status = -1;
+
+function action(mode, type, selection) {
+	status++;
+	var i = -1;
+	if (status <= i++) {
+		ms.dispose();
+	} else if (status === i++) {
+		// 初始化
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(true, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
+		ms.inGameDirectionEvent_AskAnswerTime(30);
+	} else if (status === i++) {
+		ms.fieldEffect_InsertCanvas(1, 128, 0, 0, 0, 1000, 0);
+		ms.inGameDirectionEvent_AskAnswerTime(1000);
+	} else if (status === i++) {
+		ms.inGameDirectionEvent_Monologue("#fn黑体##fs32#B - 07 F\r\n\r\n#fs22#在黑魔法师的影响下，变得狂暴的猴子们正在攻击人类。\r\n\r\n啊，那边有人受到了猴子的攻击，看那边！", 30000);
+	} else if (status === i++) {
+		ms.fieldEffect_InsertCanvas(0, 0, 0, 0, 1000, 0, 0);
+		ms.inGameDirectionEvent_PushMoveInfo(0, 600, 0, -1000);
+		ms.inGameDirectionEvent_AskAnswerTime(2000);
+	} else if (status === i++) {
+		ms.inGameDirectionEvent_PushMoveInfo(1, 0, 0, 0);
+		ms.inGameDirectionEvent_AskAnswerTime(1000);
+	} else if (status === i++) {
+		ms.fieldEffect_InsertCanvas(1, 128, 0, 0, 0, 1000, 0);
+		ms.inGameDirectionEvent_AskAnswerTime(1000);
+	} else if (status === i++) {
+		ms.inGameDirectionEvent_Monologue("#fn黑体##fs22#你去将所有的猴子驱逐出去，然后救助那里的人们。我随后会为你打开通往下一层的门。\r\n\r\n如果受伤的探险家死去的话，你将无法进入下一层，所以要注意。", 30000);
+	} else if (status === i++) {
+		ms.askYesNoS("#b（要不要问问需要捕捉多少个呢？）");
+	} else if (status === i++) {
+		ms.sendNextNoESC("左，右，左，右……我数数……", 2540000);
+	} else if (status === i++) {
+		ms.sendNextSNoESC("#b（不管了，我先上了！）");
+	} else if (status === i++) {
+		// 收尾
+		ms.fieldEffect_InsertCanvas(0, 0, 0, 0, 1000, 0, 0);
+		ms.curNodeEventEnd(true);
+		ms.setInGameDirectionMode(false, true); //屏蔽/解锁操作台 true = 锁 false = 解
+		ms.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
+		ms.dispose();
+		// TODO
+		ms.getMap().startMapEffect("请你守护好那名被狂暴的猴子伤害的探险家。", 5120085);
+		ms.warp(992007000, 1);
+	} else {
+		ms.dispose();
+	}
+}
