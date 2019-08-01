b04a7080e27ce5cfccf43d2f4f9c1182d7fc12f1
Hongyu Wang <124439607@qq.com>
2019-08-01 17:15:04 +0800

    Update StageOut.js

--- 传送点/StageOut.js
+++ 传送点/StageOut.js
@@ -5,6 +5,7 @@
 		var em = pi.getEventManager("副本_起源之塔");
 		var prop = em.getProperty("stage"+level);
 		if (prop != null && prop.equals("clear")) {
+			pi.playerMessage(-1, "传送到起源之塔的第"+(level+1)+"层。");
 		    pi.warp(mapId==992050000?992000000:mapId+1000, 0);
 		}else{
 			pi.playerMessage(-1, "传送阵目前还处于无法使用的状态。");
