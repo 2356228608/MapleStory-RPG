8f75efd0f76a89d00e1c2c9c8002b99d92f6671d
Hongyu Wang <124439607@qq.com>
2019-08-08 19:28:04 +0800

    Update stageEnter_32.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_32.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_32.js
@@ -4,6 +4,7 @@ Made by Jessefjxm
 var status = -1;
 var level = "32";
 var header = "#fn黑体##fs32#B - " + level + " F\r\n\r\n";
+var pos = [[0, 2000], [1000, 2000], [2000,2000]];
 
 function action(mode, type, selection) {
 	status++;
@@ -22,12 +23,20 @@ function action(mode, type, selection) {
 		ms.curNodeEventEnd(true);
 		ms.setInGameDirectionMode(true, true); //屏蔽/解锁操作台 true = 锁 false = 解
 		ms.setStandAloneMode(true); //屏蔽/解锁 剧情其他玩家
+		// 刷怪
+		var map = em.getMapFactoryMap(992032000);
+		for(var k=0;k<pos.length;k++){
+			for (var i = 0; i < 10; i++) {
+				var mob = em.getMonster(mobId[index]);
+				map.spawnMonsterOnGroundBelow(mob, new java.awt.Point(pos[k][0]+randomNum(-100,100), pos[k][1]));
+			}
+		}
 		ms.inGameDirectionEvent_AskAnswerTime(30);
 	} else if (status === i++) {
 		ms.fieldEffect_InsertCanvas(1, 128, 0, 0, 0, 1000, 0);
 		ms.inGameDirectionEvent_AskAnswerTime(1000);
 	} else if (status === i++) {
-		ms.inGameDirectionEvent_Monologue(header + "#fs22#这个地方跟刚刚离开的31楼区域感觉很像吧？ \r\n\r\n但是这个地方里除了最下方踏板外，上方的踏板是无法站上去的。 \r\n\r\n踩过棘草消灭上方全部的蓝色古代绿水灵！", 30000);
+		ms.inGameDirectionEvent_Monologue(header + "#fs22#这个地方跟刚刚离开的31楼区域感觉很像吧？\r\n\r\n但是这个地方里除了最下方踏板外，上方的踏板是无法站上去的。\r\n\r\n踩过棘草消灭上方全部的蓝色古代绿水灵吧！", 30000);
 	} else if (status === i++) {
 		ms.fieldEffect_InsertCanvas(0, 0, 0, 0, 1000, 0, 0);
 		// 收尾
@@ -40,5 +49,19 @@ function action(mode, type, selection) {
 		//ms.warp(992019000, 1);
 	} else {
 		ms.dispose();
+	}
+}
+
+function randomNum(minNum, maxNum) {
+	switch (arguments.length) {
+	case 1:
+		return parseInt(Math.random() * minNum + 1, 10);
+		break;
+	case 2:
+		return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+		break;
+	default:
+		return 0;
+		break;
 	}
 }
