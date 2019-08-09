e7841abfa6df7c05cb82a68933c4cc290f4ad8d4
Hongyu Wang <124439607@qq.com>
2019-08-09 13:07:31 +0800

    Update stageEnter_33.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_33.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_33.js
@@ -37,7 +37,7 @@ function action(mode, type, selection) {
 		ms.setStandAloneMode(false); //屏蔽/解锁 剧情其他玩家
 		ms.dispose();
 		em.setProperty("stage" + level, "start");
-		ms.addPopupSay(2540000, 6000, "将传送阵转到想要的方向后前进至下一层吧。");
+		ms.addPopupSay(2540000, 6000, "按照你自己的意愿转动传送口，然后前往下一层吧。");
 		ms.fieldEffect_ScreenMsg("UI/UIWindowPL.img/HiddenCatch/StageImg/start");
 		//ms.warp(992019000, 1);
 	} else {
