24a370f4bd2346b39de2d0c2f2333fa871d5bec0
Hongyu Wang <124439607@qq.com>
2019-08-09 13:44:33 +0800

    Update stageEnter_36.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_36.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_36.js
@@ -37,7 +37,6 @@ function action(mode, type, selection) {
 		ms.dispose();
 		em.setProperty("stage" + level, "start");
 		ms.addPopupSay(2540000, 6000, "你试着和跟菜鸟冒险家对话吧。");
-		ms.fieldEffect_ScreenMsg("UI/UIWindowPL.img/HiddenCatch/StageImg/start");
 		//ms.warp(992019000, 1);
 	} else {
 		ms.dispose();
