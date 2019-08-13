ca32039ac2f2dd328054b0dd133f99c74356e90a
Hongyu Wang <124439607@qq.com>
2019-08-13 13:22:36 +0800

    Update stageEnter_11.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_11.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_11.js
@@ -13,6 +13,13 @@ function action(mode, type, selection) {
 		ms.dispose();
 		return;
 	}
+	if (ms.isQuestFinished(42010)) {
+		em.setProperty("stage" + level, "start");
+		ms.addPopupSay(2540000, 6000, "请消灭300个石头人。");
+		ms.fieldEffect_ScreenMsg("UI/UIWindowPL.img/HiddenCatch/StageImg/start");
+		ms.dispose();
+		return;
+	}
 
 	var i = -1;
 	if (status <= i++) {
