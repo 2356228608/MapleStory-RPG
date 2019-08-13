72a2449cb229c50464b9beecc39fcbc1c4c37d2d
Hongyu Wang <124439607@qq.com>
2019-08-13 13:19:55 +0800

    Update stageEnter_4.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_4.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_4.js
@@ -13,6 +13,14 @@ function action(mode, type, selection) {
 		ms.dispose();
 		return;
 	}
+	if (ms.isQuestFinished(42010)) {
+		em.setProperty("stage" + parseInt(level), "start");
+		ms.warp(992004000, 1);
+		ms.addPopupSay(2540000, 6000, "请保持左右两侧怪物的平衡。");
+		ms.fieldEffect_ScreenMsg("UI/UIWindowPL.img/HiddenCatch/StageImg/start");
+		ms.dispose();
+		return;
+	}
 
 	var i = -1;
 	if (status <= i++) {
