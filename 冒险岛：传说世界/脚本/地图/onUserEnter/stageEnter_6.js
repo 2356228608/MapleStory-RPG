7262a863d71ee92e1504200f7224379f361d92ba
Hongyu Wang <124439607@qq.com>
2019-08-13 13:20:41 +0800

    Update stageEnter_6.js

--- 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_6.js
+++ 冒险岛：传说世界/脚本/地图/onUserEnter/stageEnter_6.js
@@ -13,6 +13,14 @@ function action(mode, type, selection) {
 		ms.dispose();
 		return;
 	}
+	if (ms.isQuestFinished(42010)) {
+		ms.warp(992006000, 1);
+		em.setProperty("stage" + parseInt(level), "start");
+		ms.addPopupSay(2540000, 6000, "请消灭300个绿水灵。");
+		ms.fieldEffect_ScreenMsg("UI/UIWindowPL.img/HiddenCatch/StageImg/start");
+		ms.dispose();
+		return;
+	}
 
 	var i = -1;
 	if (status <= i++) {
