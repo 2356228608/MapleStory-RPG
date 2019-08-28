9ead6517161183016de09309aeeb73863eb02e96
Hongyu Wang <124439607@qq.com>
2019-08-28 13:35:56 +0800

    Update 500650.js

--- 冒险岛：传说世界/脚本/任务/500650.js
+++ 冒险岛：传说世界/脚本/任务/500650.js
@@ -12,9 +12,7 @@ function start(mode, type, selection) {
 	if (status <= i++) {
 		qm.dispose();
 	} else if (status === i++) {
-		qm.askMenu("去#b<操作之神>#k挑战#e神之操作#n吧！\r\n\r\n#b每周三上午10点#k，可以获得上周在<操作之神>中获得的#b星星数量10倍#k的#b大冒险币#k！\r\n\r\n#e[本周奖励]#n\r\n#b大冒险币0个#k（★ 0个）\r\n\r\n#e[下周可获得奖励]#n\r\n#b大冒险币0个#k（★ 0个）\r\n\r\n#L0# #b领取本周<操作之神>奖励#k#l", 9062148);
-	} else if (status === i++) {
-		qm.sendOk("本周在<操作之神>中没有获得星星！\r\n下周一定要好好努力！", 9062148);
+		qm.askMenu("去#b<操作之神>#k挑战#e神之操作#n吧！\r\n\r\n#b每通过一关，都可以获得在<操作之神>中获得的#b星星数量5倍#k的#b大冒险币#k！", 9062148);
 	} else if (status == i++) {
 		qm.dispose();
 	}
