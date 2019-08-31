211cec0e6d970685458970cc4636f3621e8462de
Hongyu Wang <124439607@qq.com>
2019-08-31 15:35:44 +0800

    Create banban_Summon.js

---
+++ 冒险岛：传说世界/脚本/地图/onFirstUserEnter/banban_Summon.js
@@ -0,0 +1,28 @@
+// 全局变量
+var status = -1; // status: 当前聊天交互轮数
+var selectionLog = new Array(); // 记录每一轮的选择
+
+// 开头
+
+function action(mode, type, selection) {
+	if (status == 0 && mode == 0) {
+		ms.dispose();
+		return;
+	}
+	(mode == 1) ? status++ : status--;
+	selectionLog[status] = selection;
+	var i = -1;
+	if (status <= i++) {
+		ms.dispose();
+	} else if (status === i++) {
+		var eim = cm.getEventInstance();
+		if (eim != null) {
+			var em = eim.getEventManager();
+			ms.getMap().spawnMonsterOnGroundBelow(
+				em.getMonster(8910000), new java.awt.Point(256, 443));
+		}
+		ms.dispose();
+	} else {
+		ms.dispose();
+	}
+}
