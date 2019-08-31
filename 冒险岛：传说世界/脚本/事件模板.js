503ffed549164ba9f985b745896d3326c31c1131
Hongyu Wang <124439607@qq.com>
2019-08-31 15:48:08 +0800

    Create 事件模板.js

---
+++ 冒险岛：传说世界/脚本/事件模板.js
@@ -0,0 +1,174 @@
+/*  This is originally mada by 娜娜
+ *  This source is made by BMS Team
+ *  功能：模板
+ */
+
+// 自定义复活次数
+var reviveCount = 5;
+// 涵盖地图，离开这个范围会离开事件
+var mapIds = [211070100, 211070101, 211070110];
+// 大厅，离开后传送到这里
+var mapHall = 211070000;
+// 事件名称，需要在 bmsconfig.ini 里面加入才有效
+var eventName = "事件模板";
+
+function init() {
+	em.setProperty("leader", "true");
+	em.setProperty("state", "0");
+}
+
+function setup(eim, leaderid) {
+	em.setProperty("state", 1);
+	em.setProperty("leader", "true");
+	var eim = em.newInstance(eventName);
+	for (var i = 0; i < mapIds.length; i++) {
+		var map = eim.setInstanceMap(mapIds[i]);
+		map.resetFully();
+		map.killAllMonsters(false);
+	}
+	return eim;
+}
+
+function playerEntry(eim, player) {
+	var map = eim.getMapInstance(0);
+	player.changeMap(map, map.getPortal(0));
+	eim.startEventTimer(10 * 60 * 1000);
+}
+
+function playerRevive(eim, player) {
+	return false;
+}
+
+function changedMap(eim, player, mapid) {
+	// em.broadcastServerMsg("[changedMap]");
+	if (mapIds.indexOf(mapid) < 0) {
+		on玩家退场(eim, player, false);
+		return;
+	}
+	// 初始化变量
+	initProp("kill", 0);
+}
+
+function playerDisconnected(eim, player) {
+	// todo 干脆放弃？
+	// em.broadcastServerMsg("[playerDisconnected]");
+	on玩家退场(eim, player, false);
+	return 0;
+}
+
+function scheduledTimeout(eim) {
+	// em.broadcastServerMsg("[scheduledTimeout]");
+	on玩家退场(eim, eim.getPlayers().get(0), true);
+}
+
+function playerExit(eim, player) {
+	// em.broadcastServerMsg("[playerExit]");
+	on玩家退场(eim, player, false);
+}
+
+function allMonstersDead(eim) {
+	// em.broadcastServerMsg("[allMonstersDead]");
+}
+
+function monsterValue(eim, mobId) {
+	// em.broadcastServerMsg("[monsterValue]=" + mobId);
+	var kilReq = 10000;
+	var kill = parseInt(em.getProperty("kill")) + 1;
+	em.setProperty("kill", kill);
+	if (kill >= kilReq) {
+		eim.getPlayers().forEach(function (player) {
+			player.openNpc(2540005, "特效_完成");
+		});
+		em.setProperty("state", 10);
+	}
+	return 1;
+}
+
+function monsterDrop(eim, player, mob) {
+	// em.broadcastServerMsg("[monsterDrop] " + mob);
+}
+
+function allMonstersDead(eim) {
+	// em.broadcastServerMsg("[allMonstersDead] ");
+}
+
+function playerRevive(eim, player) {
+	return false;
+}
+
+function clearPQ(eim) {
+	scheduledTimeout(eim);
+}
+function leftParty(eim, player) {}
+function disbandParty(eim) {}
+function playerDead(eim, player) {
+	// em.broadcastServerMsg("[playerDead]");
+}
+function cancelSchedule() {}
+
+var setupTask;
+
+// ===================== 功能类方法 ======================
+
+function randomNum(minNum, maxNum) {
+	switch (arguments.length) {
+	case 1:
+		return parseInt(Math.random() * minNum + 1, 10);
+		break;
+	case 2:
+		return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+		break;
+	default:
+		return 0;
+		break;
+	}
+}
+
+// 初始化参数
+function initProp(name, val) {
+	if (em.getProperty(name) == null)
+		em.setProperty(name, val);
+}
+function initPropArray(name, val, startI, endI) {
+	for (var i = startI; i <= endI; i++) {
+		if (em.getProperty(name + "_" + i) == null)
+			em.setProperty(name + "_" + i, val);
+	}
+}
+// 随机数序列
+function initPropArrayRandom(name, min, max, startI, endI) {
+	var arr = new Array();
+	var maxTimes = endI - startI + 1;
+	var canRepeat = (max - min) < (endI - startI) ? true : false;
+	do {
+		var num = randomNum(min, max);
+		if (canRepeat || arr.indexOf(num) < 0) { //数组中不存在
+			arr.push(num);
+			maxTimes--;
+		}
+	} while (maxTimes);
+	for (var i = startI; i <= endI; i++) {
+		if (em.getProperty(name + "_" + i) == null)
+			em.setProperty(name + "_" + i, arr[i - 1]);
+	}
+}
+// 循环调动事件
+function scheduleNew(funcName, seconds) {
+	var setupTask = em.schedule(funcName, seconds * 1000);
+	return setupTask;
+}
+// 处理玩家退场
+function on玩家退场(eim, player, isTimeout) {
+	// ????
+	if (isTimeout) {
+		eim.disposeIfPlayerBelow(100, mapHall);
+	} else {
+		eim.disposeIfPlayerBelow(0, 0);
+	}
+	// 清空配置文件
+	em.getProperties().clear();
+	em.setProperty("state", 0);
+	em.setProperty("leader", "true");
+	eim.unregisterPlayer(player);
+}
+
