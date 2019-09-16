1d849e226003054a062fcd3bc47c764170fa6740
jessefjxm <124439607@qq.com>
2019-09-16 22:53:20 +0800

    日常更新

---
+++ 传送点/pt_henespin.js
@@ -0,0 +1,5 @@
+function enter(pi) {
+	var id = pi.getPortal().getId();
+	var name = pi.getPortalName();
+	pi.playerMessage(5, "这个传送点 [" + id + " : " + name + "] 的脚本尚未修复。脚本位于： 脚本/传送点/pt_henespin.js 【注意】修改完脚本后要在控制台重载才能生效！");
+}
